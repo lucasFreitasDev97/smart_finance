430e36615649b5f6bb3322b2aa67cf4d87050d36
Lucas Freitas <lucas.rodrigues.developer97@gmail.com>
2025-10-23 15:25:43 -0300

    cria models e seus respectivos relacionamentos referentes as tabelas accounts, categories e transactions

---
+++ app/Models/Transaction.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Transaction extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['user_id', 'account_id', 'category_id', 'description', 'amount', 'date'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function account(): BelongsTo
+    {
+        return $this->belongsTo(Account::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
